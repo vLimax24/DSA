5c52486dd4a6818ad0907bf5c62d5f82e8d3aadf
Limax <limax.workspace@gmail.com>
2026-01-21 20:52:28 +0100

    aufgabe: verdoppeln gerader Zahlen in Array

---
+++ Aufgabe2.py
@@ -0,0 +1,14 @@
+# Imperative schreibweise
+
+def verdopple_gerade(zahlen):
+    result = []
+    for z in zahlen:
+        if z % 2 == 0:
+            result.append(z * 2)
+        else:
+            result.append(z)
+    return result
+
+#Test:
+print(verdopple_gerade([1, 2, 3, 4, 5]))  # Sollte [1, 4, 3, 8, 5] ausgeben
+print(verdopple_gerade([10, 15, 20]))     # Sollte [20, 15, 40] ausgeben
