8d0bda9425e1d9a3f40a47d0d7057c204fdfe989
Limax <limax.workspace@gmail.com>
2026-01-21 20:56:57 +0100

    update: optimierte Schreibweise für Aufgabe2

--- Aufgabe2.py
+++ Aufgabe2.py
@@ -12,3 +12,15 @@ def verdopple_gerade(zahlen):
 #Test:
 print(verdopple_gerade([1, 2, 3, 4, 5]))  # Sollte [1, 4, 3, 8, 5] ausgeben
 print(verdopple_gerade([10, 15, 20]))     # Sollte [20, 15, 40] ausgeben
+
+# deklerative Schreibweise (List Comprehensions)
+
+# Wenn nur if: [ausdruck -> for -> if]
+# Wenn if-else: [ausdruckTrue -> if -> bedingung -> else -> ausdruckFalse -> for]
+
+def verdopple_gerade(zahlen):
+    return [z *  2 if z % 2 == 0 else z for z in zahlen]
+
+#Test:
+print(verdopple_gerade([1, 2, 3, 4, 5]))  # Sollte [1, 4, 3, 8, 5] ausgeben
+print(verdopple_gerade([10, 15, 20]))     # Sollte [20, 15, 40] ausgeben
